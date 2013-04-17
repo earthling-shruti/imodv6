08d0add6a04465d58e7f6bedff11ee2b03053ab1
earthling-shruti <earthling.shruti@gmail.com>
2013-04-16 20:58:19 -0700

    User roles added in Bootstrap.groovy

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,6 +2,7 @@ class BootStrap {
 
     def init = { servletContext ->
 		if (imodv6.Role.list().size() == 0) {
+			//user roles can be changed here, or in the ui
 			new imodv6.Role(authority:"ROLE_ANONYMOUS").save();
 			new imodv6.Role(authority:"ROLE_ADMIN").save();
 			new imodv6.Role(authority:"ROLE_USER").save();
